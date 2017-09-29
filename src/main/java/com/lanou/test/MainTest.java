a13de1ac74904fe9ddd124c5b131f9d7d06772b1
zar <a18502436094@163.com>
2017-09-29 08:53:20 +0800

    2017-09-29 8:53:10创建的新工程

---
+++ src/main/java/com/lanou/test/MainTest.java
@@ -0,0 +1,10 @@
+package com.lanou.test;
+
+/**
+ * Created by zar on 2017/9/29.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
